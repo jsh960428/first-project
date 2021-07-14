69ed49e27c2a51ac597dc14ef61245261921f9b4
jsh960428 <87401594+jsh960428@users.noreply.github.com>
2021-07-14 10:46:56 +0900

    Add files via upload

---
+++ pf-02/script/script.js
@@ -0,0 +1,9 @@
+$(function(){
+    $(".menu>ul>li").hover(function(){
+        $(this).find(".sub").stop().slideDown(500);
+    },
+    function(){
+        $(this).find(".sub").stop().slideUp(500);
+    });
+
+});
