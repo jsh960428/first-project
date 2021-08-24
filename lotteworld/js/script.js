8a9f57942c334500ca8aabd3e83af6579825f8b4
jsh960428 <87401594+jsh960428@users.noreply.github.com>
2021-08-24 15:31:39 +0900

    Add files via upload

--- lotteworld/js/script.js
+++ lotteworld/js/script.js
@@ -18,4 +18,63 @@ $(function(){
     $(".nextBtn").on("click", function(){
         mySwipe.next(); //다음 배너로 이동
     });
+
+    $(".menu>ul>li:nth-child(1)").on("click", function () {
+        var about = $("#slider").position();
+        $("html, body").animate({
+            scrollTop: about.top - 80
+        }, 400);
+    });
+    $(".menu>ul>li:nth-child(2)").on("click", function () {
+        var about = $(".info").position();
+        $("html, body").animate({
+            scrollTop: about.top - 25
+        }, 400);
+    });
+    $(".menu>ul>li:nth-child(3)").on("click", function () {
+        var about = $(".play").position();
+        $("html, body").animate({
+            scrollTop: about.top - 80
+        }, 400);
+    });
+    $(".menu>ul>li:nth-child(4)").on("click", function () {
+        var about = $(".event").position();
+        $("html, body").animate({
+            scrollTop: about.top - 50
+        }, 400);
+    });
+    $(".menu>ul>li:nth-child(5)").on("click", function () {
+        var about = $("section.help").position();
+        $("html, body").animate({
+            scrollTop: about.top - 20
+        }, 400);
+    });
+
+
+    $(window).scroll(function () {
+        var height = $(document).scrollTop();
+        if (height <= 660) {
+            $(".menu>ul>li").removeClass("on");
+            $(".menu>ul>li:nth-child(1)").addClass("on");
+
+        } else if (height >= 661 && height <= 1950) {
+            $(".menu>ul>li").removeClass("on");
+            $(".menu>ul>li:nth-child(2)").addClass("on");
+
+        } else if (height >= 1951 && height <= 3053) {
+            $(".menu>ul>li").removeClass("on");
+            $(".menu>ul>li:nth-child(3)").addClass("on");
+
+        } else if (height >= 3054 && height <= 4345) {
+            $(".menu>ul>li").removeClass("on");
+            $(".menu>ul>li:nth-child(4)").addClass("on");
+
+        } else if (height >= 4346) {
+            $(".menu>ul>li").removeClass("on");
+            $(".menu>ul>li:nth-child(5)").addClass("on");
+
+        } else {
+            $(".menu>ul>li").removeClass("on");
+        }
+    });
 });
